c3b4266b9e6c0496bebeeb1a4158be6d5563ac49
Ksiks Mohamed Haitam <simpleteam2001@gmail.com>
2026-04-04 20:51:30 +0200

    add init virtual metho

--- io/graphics/oam/ObjectAttributesMemory.cpp
+++ io/graphics/oam/ObjectAttributesMemory.cpp
@@ -13,7 +13,7 @@ namespace GBE
 
     void ObjectAttributesMemory::Init()
     {
-        SetReadWriteFlags(true);
+        // SetReadWriteFlags(true);
     }
 
     void ObjectAttributesMemory::_SetImp(uint16_t address, uint8_t value)
